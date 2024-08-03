0fef2b527ede5e3d0995032c3d20deaa6a7f5b7f
Jehovanie RAM <jehovanieram@gmail.com>
2024-08-03 13:55:32 +0300

    Install tailwind and add flowbite module

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import router from "./router/@config/router_config.tsx";
 import { RouterProvider } from "react-router-dom";
 
+import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<RouterProvider router={router} />
--- src/router/public/Home.tsx
+++ src/router/public/Home.tsx
@@ -1,5 +1,12 @@
+import Alert from "../../@share/components/Alert";
+
 const Home = () => {
-	return <div>Home...</div>;
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<Alert />
+		</div>
+	);
 };
 
 export default Home;
