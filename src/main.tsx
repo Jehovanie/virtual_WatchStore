b4191b7f7e0b5499219fb302b5e5ce30708ab439
Jehovanie <jehovanieram@gmail.com>
2024-08-07 16:26:03 +0200

    Setting redux toolkit: start

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom/client";
 import router from "./router/@config/router_config.tsx";
 import { RouterProvider } from "react-router-dom";
 
+import { store } from "./_config/app/store.ts";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import "flowbite";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+  		</Provider>
 	</React.StrictMode>
 );
