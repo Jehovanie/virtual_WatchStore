2164bfa9bf72b4605e65b9f816fd114842db0967
Jehovanie RAM <jehovanieram@gmail.com>
2024-08-21 06:07:25 +0300

    Fix error endPoint

--- src/modules/details/components/_shared/ItemRelatedProduct.tsx
+++ src/modules/details/components/_shared/ItemRelatedProduct.tsx
@@ -4,6 +4,7 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 import image_2 from "@assets/images/image_2.jpg";
 import { WatchModel } from "@/_shared/models";
 import { Link } from "react-router-dom";
+import Price from "@/_shared/components/Price";
 
 interface WatchProps {
 	watch: WatchModel;
@@ -31,11 +32,12 @@ const ItemRelatedProduct: React.FC<WatchProps> = ({ watch }) => {
 					</span>
 				</div>
 				<div className="flex items-center justify-between">
-					<span className="text-3xl font-bold text-gray-900 dark:text-white">${watch.price.toString()}</span>
+					<span className="text-3xl font-bold text-gray-900 dark:text-white">
+						<Price price={parseFloat(watch.price.toString())} className="" />
+					</span>
 					<a
 						href="#"
-						className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-thin text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-					>
+						className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-thin text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
 						Add to cart
 					</a>
 				</div>
